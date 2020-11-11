5f7cf0d4d2f4f413a41113286c185173a685d3e0
ivan.chadin <ivan.chadin@exactprosystems.com>
2020-11-11 15:42:55 +0400

    TH2-801 [RECON] updated recon to support multiple queues
    Fix sending EventBatch

--- th2_check2_recon/services.py
+++ th2_check2_recon/services.py
@@ -50,7 +50,7 @@ class EventsBatchCollector(AbstractService):
             if event.parent_id.id in self.__batches:
                 event_batch, batch_timer = self.__batches.get(event.parent_id.id)
             else:
-                logger.info(f"parent_id.id is None for {event.id.id}")
+                logger.info(f"{event} is {event.parent_id.id is None} has {event.HasField('parent_id')}")
                 if event.parent_id.id is None:
                     event_batch = EventBatch()
                     event_batch.events.append(event)
