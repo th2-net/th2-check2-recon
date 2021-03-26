fa19a1b02c301a530057bcc1b499434d2f32dd88
Ivan Chadin <vaaaaaanya@gmail.com>
2021-03-26 15:47:45 +0400

    Update requirements.txt

--- th2_check2_recon/recon.py
+++ th2_check2_recon/recon.py
@@ -15,9 +15,6 @@
 import asyncio
 import importlib
 import logging
-import signal
-import threading
-import time
 from typing import Optional
 
 from th2_common.schema.event.event_batch_router import EventBatchRouter
@@ -36,7 +33,6 @@ class Recon:
         logger.info('Recon initializing...')
         self.rules = []
         self.__loop = asyncio.get_event_loop()
-        self.__loop.add_signal_handler(signal.SIGTERM, self.__handler, self.__loop)
         self.__config = ReconConfiguration(**custom_config)
         self.__message_router = message_router
 
@@ -45,47 +41,36 @@ class Recon:
                                       report_name=self.__config.recon_name,
                                       event_batch_max_size=self.__config.event_batch_max_size,
                                       event_batch_send_interval=self.__config.event_batch_send_interval)
-        self.__recon_lock = threading.Lock()
-        self.__is_stopped = None
-
-    def __handler(self, loop):
-        logger.info('SIGTERM received in recon')
-        loop.remove_signal_handler(signal.SIGTERM)
-        loop.call_soon_threadsafe(loop.stop)
-        logger.info('Recon event_loop is stopped')
-        self.stop()
 
     def start(self):
-        logger.info('Recon running...')
-        self.rules = self.__load_rules()
-        for rule in self.rules:
-            for attrs in rule.get_attributes():
-                self.__message_router.subscribe_all(rule.get_listener(), *attrs)
-        logger.info('Recon started!')
-        self.__is_stopped = False
-        self.__loop.run_forever()
+        try:
+            logger.info('Recon running...')
+            self.rules = self.__load_rules()
+            for rule in self.rules:
+                for attrs in rule.get_attributes():
+                    self.__message_router.subscribe_all(rule.get_listener(), *attrs)
+            logger.info('Recon started!')
+            self.__loop.run_forever()
+        except Exception:
+            logger.info('Recon work interrupted')
 
     def stop(self):
-        with self.__recon_lock:
-            if self.__is_stopped:
-                return
-            logger.info('Recon try to stop')
+        logger.info('Recon try to stop')
+        try:
+            self.__message_router.unsubscribe_all()
+            for rule in self.rules:
+                rule.stop()
+            if self.message_comparator is not None:
+                self.message_comparator.stop()
+            self.event_store.stop()
+        except Exception:
+            logger.exception('Error while stop Recon')
+        finally:
             try:
-                self.__message_router.unsubscribe_all()
-                for rule in self.rules:
-                    rule.stop()
-                if self.message_comparator is not None:
-                    self.message_comparator.stop()
-                self.event_store.stop()
-            except Exception:
-                logger.exception('Error while stop Recon')
-            finally:
-                while self.__loop.is_running():
-                    logger.warning('Recon event loop cannot be closed because it is running. Try in 1 sec.')
-                    time.sleep(1)
                 self.__loop.close()
-                self.__is_stopped = True
-                logger.info('Recon stopped!')
+            except Exception:
+                pass
+            logger.info('Recon stopped!')
 
     def __load_rules(self):
         logger.info('Try load rules')
