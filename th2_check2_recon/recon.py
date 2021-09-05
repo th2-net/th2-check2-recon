f478eb7ed7b0c85ce3b7ae58aa4b67cf53eae331
Ivan Chadin <vaaaaaanya@gmail.com>
2021-09-06 01:14:16 +0400

    [TS-603] Add values to 'type' column for th2-check2-recon box

--- th2_check2_recon/recon.py
+++ th2_check2_recon/recon.py
@@ -27,7 +27,6 @@ from th2_check2_recon.handler import GRPCHandler
 from th2_check2_recon.reconcommon import MessageGroupType, ReconMessage
 from th2_check2_recon.services import EventStore, MessageComparator
 
-
 logger = logging.getLogger(__name__)
 
 
