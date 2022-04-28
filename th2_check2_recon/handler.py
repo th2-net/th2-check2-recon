eca27f6aaa8c1f6bdb949e77c334dfdea96dbfd4
vyacheslav.svirkov <vyacheslav.svirkov@exactprosystems.com>
2022-04-28 13:54:59 +0300

    [TH2-2092] Crawler connection.

--- th2_check2_recon/handler.py
+++ th2_check2_recon/handler.py
@@ -11,17 +11,21 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+import json
 import logging
 import time
 from abc import ABC, abstractmethod
 
+import google.protobuf.empty_pb2
 from google.protobuf.text_format import MessageToString
 from th2_common.schema.message.message_listener import MessageListener
-from th2_grpc_check2_recon.check2_recon_pb2_grpc import Check2ReconServicer
-from th2_grpc_common.common_pb2 import MessageBatch, RequestStatus
+from th2_grpc_common.common_pb2 import MessageBatch, MessageGroupBatch
+from th2_grpc_crawler_data_processor.crawler_data_processor_pb2 import DataProcessorInfo, EventResponse, Status, \
+    MessageResponse
+from th2_grpc_crawler_data_processor.crawler_data_processor_pb2_grpc import DataProcessorServicer
 
 from th2_check2_recon.common import MessageUtils
+from th2_check2_recon.recon import Recon
 from th2_check2_recon.reconcommon import ReconMessage
 
 logger = logging.getLogger(__name__)
@@ -60,26 +64,36 @@ class MessageHandler(AbstractHandler):
                              f'Message: {MessageToString(batch, as_one_line=True)}')
 
 
-class GRPCHandler(Check2ReconServicer):
+class GRPCHandler(DataProcessorServicer):
 
-    def __init__(self, rules: list) -> None:
-        self._rules = rules
+    def __init__(self, recon: Recon) -> None:
+        self._recon = recon
 
-    def submitGroupBatch(self, request, context):
+    def CrawlerConnect(self, request, context):
+        logger.debug('CrawlerId {0} connected'.format(request.id.name))
+        version = json.loads(open('../package_info.json').read())['package_version']
+        return DataProcessorInfo(name='Recon', version=version)
+
+    def IntervalStart(self, request, context):
+        logger.debug('Interval set from {0} to {1}'.format(request.start_time, request.end_time))
+        return google.protobuf.empty_pb2.Empty()
+
+    def SendEvent(self, request, context):
+        logger.debug('CrawlerID {0} sent events {1}'.format(request.id.name, MessageToString(request.event_data, as_one_line=True)))
+        return EventResponse(id=self._recon.event_store.root_event.id, status=Status(handshake_required=False))
+
+    def SendMessage(self, request, context):
         try:
-            logger.debug(f'submitGroupBatch request: {MessageToString(request, as_one_line=True)}')
-
-            messages = [message.message for group in request.groups
-                        for message in group.messages if message.HasField('message')]
-
+            logger.debug('CrawlerID {0} sent messages {1}'.format(request.id.name, MessageToString(request.message_data, as_one_line=True)))
+            batch = MessageGroupBatch()
+            batch.ParseFromString(request.message_data.body_raw)
+            messages = [any_message.message for group in batch.groups
+                        for any_message in group.messages if any_message.HasField('message')]
             for proto_message in messages:
                 message = ReconMessage(proto_message=proto_message)
-
-                for rule in self._rules:
-
+                for rule in self._recon.rules:
                     process_timer = rule.RULE_PROCESSING_TIME
                     start_time = time.time()
-
                     try:
                         rule.process((), message)
                     except Exception:
@@ -88,11 +102,9 @@ class GRPCHandler(Check2ReconServicer):
                                          f'Message: {MessageToString(proto_message, as_one_line=True)}')
                     finally:
                         process_timer.observe(time.time() - start_time)
-
                 logger.debug(f"Processed '{proto_message.metadata.message_type}' "
                              f"id='{MessageUtils.str_message_id(proto_message)}'")
-
-            return RequestStatus(status=RequestStatus.SUCCESS, message='Successfully processed batch')
+            return MessageResponse(ids=[msg.metadata.id for msg in messages], status=Status(handshake_required=False))
         except Exception as e:
-            logger.exception('submitGroupBatch request failed')
-            return RequestStatus(status=RequestStatus.ERROR, message=str(e))
+            logger.exception('SendMessage request failed')
+            return MessageResponse(ids=[], status=Status(handshake_required=True))
