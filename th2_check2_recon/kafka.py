ce36c2055b116cf5e3a4264147f76ff88dadb9f2
Denis Plotnikov <denis.plotnikov@exactprosystems.com>
2023-07-19 16:40:00 +0400

    Per rule topic/key configuration

--- th2_check2_recon/kafka.py
+++ th2_check2_recon/kafka.py
@@ -21,6 +21,7 @@ class KafkaConfiguration:
     def __init__(self, config: dict):
         self.topic = config.pop("topic", "default")
         self.key = config.pop("key", 'default_key')
+        self.use_keys = config.pop("use_keys", False)
         self.fail_on_connection_failure = config.pop("fail_on_connection_failure", False)
         self.bootstrap_servers = config.get("bootstrap.servers", "localhost:9092")
         self.producer_config = config
@@ -31,7 +32,7 @@ class KafkaClient:
         pass
 
     @abstractmethod
-    def send_with_topic(self, topic: str, message: str) -> None:
+    def send_with_topic(self, topic, key, message: str) -> None:
         pass
 
     @abstractmethod
@@ -68,19 +69,28 @@ class KafkaProducer(KafkaClient):
     def _is_connected(self):
         return self.connected and self.producer != None
     
-    def send_with_topic(self, topic: str, message: str) -> None:
+    def send_with_topic(self, topic, key, message: str) -> None:
         if not self._is_connected():
             self._connect()
+
+        if key == None:
+            key = self.key
+        
+        if topic == None:
+            topic = self.topic
 
         if self._is_connected():  
             try:
                 self.producer.poll(0)
-                self.producer.produce(topic, key=self.key, value=message.encode('utf-8'), callback=self._on_delivery)
+                if self.use_keys:
+                    self.producer.produce(topic, key=self.key, value=message.encode('utf-8'), callback=self._on_delivery)
+                else:
+                    self.producer.produce(topic, value=message.encode('utf-8'), callback=self._on_delivery)
             except Exception as e:
                 logger.error(f"Error while sending kafka message: {e}", e)
     
     def send(self, message: str) -> None:
-        self.send_with_topic(self.topic, message)
+        self.send_with_topic(self.topic, self.key, message)
     
     def flush(self) -> None:
         if self.producer:
@@ -90,7 +100,7 @@ class BlobKafkaClient(KafkaClient):
     def send(self, message: str) -> None:
         pass
 
-    def send_with_topic(self, topic: str, message: str) -> None:
+    def send_with_topic(self, topic, key, message: str) -> None:
         pass
 
     def flush(self) -> None:
